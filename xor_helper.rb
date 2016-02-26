b046a78c82daf8e86cf95d935163e8a8751618d6
Soumya Ray <soumya.ray@gmail.com>
2016-02-27 00:03:46 +0800

    Full assignment description

---
+++ xor_helper.rb
@@ -0,0 +1,15 @@
+# Author: Soumya Ray (soumya.ray@gmail.com) <-- change this to your teammates!
+
+# XorHelper provides an encode method to xor a file with a key
+module XorHelper
+
+  # XOR encodes/decodes a document with a key
+  # Parameters:
+  #   doc: string
+  #   key: string
+  # Returns: string
+  def self.encode(doc, key)
+    # Enter all your code into this method (only!)
+
+  end
+end
